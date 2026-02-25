f12192b245f35090e872a4a249955d94db7cddce
joaovictor-pro <joaovictor15bazilio@gmail.com>
2026-02-25 10:03:54 -0300

    Update 2026_02_24_232027_create_habilidades_table.php

--- database/migrations/2026_02_24_232027_create_habilidades_table.php
+++ database/migrations/2026_02_24_232027_create_habilidades_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('habilidades', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 100);
+            $table->text('descricao')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+  
     public function down(): void
     {
         Schema::dropIfExists('habilidades');
