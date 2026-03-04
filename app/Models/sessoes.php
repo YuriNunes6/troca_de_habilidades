06b1870b6aa50d4395ce8354b7ce548210cb2778
joaovictor-pro <joaovictor15bazilio@gmail.com>
2026-03-04 18:34:54 -0300

    atualizações da sessões

--- app/Models/sessoes.php
+++ app/Models/sessoes.php
@@ -1,11 +1,38 @@
 <?php
 
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sessoes extends Model
+class Sessao extends Model
 {
-    use HasFactory;
+    protected $table = 'sessoes';
+
+
+    protected $fillable = [
+        'id_usuario',
+        'id_habilidade',
+        'horario_inicio',
+        'horario_fim',
+        'data',
+        'status',
+        'observacoes'
+    ];
+
+  
+
+    // Usuário da sessao
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');// Relacionamento com o modelo User, usando a chave estrangeira 'id_usuario'
+    }
+
+    // Habilidade da sessão
+    public function habilidade()
+    {
+        return $this->belongsTo(Habilidade::class, 'id_habilidade');// Relacionamento com o modelo Habilidade, usando a chave estrangeira 'id_habilidade'
+    }
 }
+
+ 
